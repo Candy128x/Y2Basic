9e3ab2cb3e9be1a3a131896710feb01f87811049
AshishSondagar <Candy128x>
2018-10-14 12:37:20 +0530

    note3 </> Sun, 2018-10-14, 12:37:20 IST 287

--- gitPushW.bash
+++ gitPushW.bash
@@ -13,7 +13,7 @@ git commit -m "$cmmt </> $(date +"%a, %Y-%m-%d, %H:%M:%S %Z %j")"
 
 git pull remty2b brny2b
 
-#git push remty2b brny2b 
-git push -f remty2b brny2b  
+git push remty2b brny2b 
+#git push -f remty2b brny2b  
 
 $SHELL
